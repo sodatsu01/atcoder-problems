3ead7adeddc54e5a03b2372974bb7a408a14f46c
sodatsu01 <darumasanga.korondaa@gmail.com>
2020-01-20 12:03:50 +0900

    D問題解説見て解けた

--- abc/152/d.rb
+++ abc/152/d.rb
@@ -1,13 +1,19 @@
 n = gets.to_i
+h = {}
+for i in 1..9
+  for j in 1..9
+    h[[i,j]] = 0
+  end
+end
+for k in 1..n
+  i = k.to_s[0].to_i
+  j = k.to_s[-1].to_i
+  h[[i,j]] += 1 if j !=0
+end
 count = 0
-for i in 1..n
-  s = i.to_s
-  if s.length == 1
-    count += 1
-  elsif s[0] == s[-1]
-    count += s.length * 2 - 1
-  elsif s[0] > s[-1] && s[-1] != 0
-    count += 11 ** (s.length - 2)
+for i in 1..9
+  for j in 1..9
+    count += h[[i,j]] * h[[j,i]]
   end
 end
 puts count
